771289a0683b3a608b6cead83e8f530baeebab51
afshinmhdz <afshinmhdz1906@gmail.com>
2024-06-03 16:17:44 +0330

    add and start two component character List and character Detail

---
+++ src/components/CharacterDetail.jsx
@@ -0,0 +1,6 @@
+function CharacterDetail(){
+    return(
+        <div></div>
+    )
+}
+export default CharacterDetail;
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,6 @@
+function CharacterList(){
+    return(
+        <div>characterList</div>
+    );
+}
+export default CharacterList;
